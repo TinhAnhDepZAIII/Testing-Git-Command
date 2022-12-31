b1ec4fbab78ed6fd873ddee92d5a222b11a88fb7
TinhAnhDepZaIII <anh.nguyennhu2306@hcmut.edu.vn>
2022-12-31 20:42:14 +0700

    update

--- Hello.cpp
+++ Hello.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 int main (){
     cout<<"hello";
-    cout<<end;
+    cout<<endl;
     cout<<"I want to be a data scientist";
+
+    cout<<"huhuhuhuhu";
+
+    dasdsa
+    __DEFINE_CPP_OVERLOAD_STANDARD_FUNC_0_0
+    dasdas
+    
+
+    
     return 0;
     
 
